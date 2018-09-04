73249d71aa8c735f6bbec509e5c80e34d0140158
Colin Walker <colinwalker270@gmail.com>
2018-09-04 14:28:31 -0700

    Add basic moves.

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
 import flask
 import json
 import logging
-import snake
 
 app = flask.Flask(__name__)
+
+
+def next_coord(coord, board, move):
+    if move == "up":
+        if coord[1] - 1 < 0:
+            return None
+        return (coord[0], coord[1] - 1)
+    elif move == "left":
+        if coord[0] - 1 < 0:
+            return None
+        return (coord[0] - 1, coord[1])
+    elif move == "right":
+        if coord[0] + 1 > len(board[coord[0]]):
+            return None
+        return (coord[0] + 1, coord[1])
+    elif move == "down":
+        if coord[1] + 1 > len(board[coord[0]]):
+            return None
+        return (coord[0], coord[1] + 1)
+
+
+def is_safe(board, coord):
+    return board[coord[0]][coord[1]] is None
+
+
+def do_move(state):
+    width = state["board"]["width"]
+    height = state["board"]["height"]
+    board = [[None for _ in range(height)] for _ in range(width)]
+
+    for coord in state["board"]["food"]:
+        board[coord["x"]][coord["y"]] = "food"
+
+    for snake in state["board"]["snakes"]:
+        for coord in snake["body"]:
+            board[coord["x"]][coord["y"]] = "snake"
+
+    head = state["you"]["body"][0]
+    me = (head["x"], head["y"])
+
+    moves = ["up", "down", "left", "right"]
+    for move in moves:
+        target = next_coord(me, board, move)
+        print(move, target)
+        if target and is_safe(board, target):
+            return move
 
 
 @app.route("/")
@@ -18,8 +63,8 @@ def start():
 
 @app.route("/move", methods=["GET", "POST"])
 def move():
-    snake.move(flask.request.json)
-    return json.dumps({"move": "up"})
+    move = do_move(flask.request.json)
+    return json.dumps({"move": move})
 
 
 @app.route("/")
@@ -32,3 +77,7 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception("An error occurred during a request.")
     return "An internal error occurred.", 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8090, debug=True)
